e92924c863aa46ba3d085c5988f2995956c99899
Elina Kamaeva <71763367+elinkamaeva@users.noreply.github.com>
2021-05-13 21:58:38 +0300

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -1,21 +1,34 @@
 import sqlite3
 
-class DBHelper:
-    def __init__(self, dbname="movies_and_users.sqlite"):
-        self.dbname = dbname
-        self.conn = sqlite3.connect(dbname)
+__connection = None
 
-    def setup(self):
-        stmt = "CREATE TABLE IF NOT EXISTS items (description text)"
-        self.conn.execute(stmt)
-        self.conn.commit()
+def ensure_connection(func):
+    def inner(*args, **kwargs):
+        with sqlite3.connect('database.db') as conn:
+            res = func(*args, conn=conn, **kwargs)
+        return res
+    return inner
 
-    def add_item(self, item_text):
-        stmt = "INSERT INTO items (description) VALUES (?)"
-        args = (item_text, )
-        self.conn.execute(stmt, args)
-        self.conn.commit()
+@ensure_connection
+def init_db(conn, force: bool = False):
+    c = conn.cursor()
+    if force:
+        c.execute('DROP TABLE IF EXISTS movies')
 
-    def get_items(self):
-        stmt = "SELECT description FROM items"
-        return [x[0] for x in self.conn.execute(stmt)]
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS movies (
+            id          INTEGER PRIMARY KEY,
+            user_id     INTEGER NOT NULL,
+            user_name   TEXT NOT NULL,
+            movie_id    INTEGER NOT NULL,
+            movie_name  TEXT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+
+@ensure_connection
+def add_information(conn, user_id: int, user_name: str, movie_id: int, movie_name: str):
+    c = conn.cursor()
+    c.execute('INSERT INTO movies (user_id, user_name, movie_id, movie_name) VALUES (?, ?, ?, ?)', (user_id, user_name, movie_id, movie_name))
+    conn.commit()
