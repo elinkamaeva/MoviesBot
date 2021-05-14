96e3eb660da4b0dda23b79710519b8d638beffc7
Elina Kamaeva <71763367+elinkamaeva@users.noreply.github.com>
2021-05-14 14:42:09 +0300

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -14,9 +14,20 @@ def init_db(conn, force: bool = False):
     c = conn.cursor()
     if force:
         c.execute('DROP TABLE IF EXISTS movies')
-
     c.execute('''
         CREATE TABLE IF NOT EXISTS movies (
+            id          INTEGER PRIMARY KEY,
+            user_id     INTEGER NOT NULL,
+            user_name   TEXT NOT NULL,
+            movie_id    INTEGER NOT NULL,
+            movie_name  TEXT NOT NULL
+        )
+    ''')
+
+    if force:
+        c.execute('DROP TABLE IF EXISTS similars')
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS similars (
             id          INTEGER PRIMARY KEY,
             user_id     INTEGER NOT NULL,
             user_name   TEXT NOT NULL,
@@ -31,4 +42,10 @@ def init_db(conn, force: bool = False):
 def add_information(conn, user_id: int, user_name: str, movie_id: int, movie_name: str):
     c = conn.cursor()
     c.execute('INSERT INTO movies (user_id, user_name, movie_id, movie_name) VALUES (?, ?, ?, ?)', (user_id, user_name, movie_id, movie_name))
+    conn.commit()
+
+@ensure_connection
+def add_similar(conn, user_id: int, user_name: str, movie_id: int, movie_name: str):
+    c = conn.cursor()
+    c.execute('INSERT INTO similars (user_id, user_name, movie_id, movie_name) VALUES (?, ?, ?, ?)', (user_id, user_name, movie_id, movie_name))
     conn.commit()
